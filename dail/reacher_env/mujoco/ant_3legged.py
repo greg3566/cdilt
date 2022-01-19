cab64275e409fe9942f6ecff1491bb333dda2d37
jeongwoo <jeongwoo.oh@rllab.snu.ac.kr>
2022-01-19 11:48:55 +0900

    add mujoco env

---
+++ dail/reacher_env/mujoco/ant_3legged.py
@@ -0,0 +1,369 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+import math
+
+class Ant3leggedForwardOriginalPenaltyEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        self.i_episode = 0
+        self.max_steps = 1000
+        self.steps = 0
+        self.theta = 0.0
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+        self.diverge_penalty_ratio = 1.0
+
+        print("START Forward original Ant")
+        mujoco_env.MujocoEnv.__init__(self, "ant_3legged.xml", 5)
+        utils.EzPickle.__init__(self)        
+
+    def step(self, a):
+        self.steps += 1
+        
+        # no goal but constrain direction
+        xb = self.get_body_com("torso")[0]
+        yb = self.get_body_com("torso")[1]
+        self.do_simulation(a, self.frame_skip)
+        xa = self.get_body_com("torso")[0]
+        ya = self.get_body_com("torso")[1]
+        dx = xa - xb
+        dy = ya - yb
+        forward_reward = (dx * self.ct + dy * self.st) / self.dt
+        vertical_cost = (abs(-xa * self.st + ya * self.ct) - abs(-xb * self.st + yb * self.ct)) / self.dt
+        if vertical_cost > 0:
+            vertical_cost = vertical_cost * self.diverge_penalty_ratio
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - vertical_cost - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        done = not notdone
+        ob = self._get_obs()
+        if self.steps % 100 == 0:
+            print("[%d] %.2f %.2f %.2f" %(self.i_episode, self.get_body_com("torso")[0], self.get_body_com("torso")[1], math.atan2(self.get_body_com("torso")[1], self.get_body_com("torso")[0])))
+        return (
+            ob,
+            reward,
+            done,
+            dict(
+                reward_forward=forward_reward,
+                reward_vertical=-vertical_cost,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+
+    def _get_obs(self):
+        # single goal
+        x = self.sim.data.qpos.flat[0]
+        y = self.sim.data.qpos.flat[1]
+        r = math.sqrt(x ** 2 + y ** 2)
+        ct = x / r
+        st = y / r
+        return np.concatenate(
+            [
+                [r, ct, st],
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
+
+    def reset_model(self):
+        self.i_episode += 1
+        self.steps = 0
+        self.theta = 0.0
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+
+        # single goal
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+class Ant3leggedForwardUpdatedPenaltyEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        self.i_episode = 0
+        self.max_steps = 1000
+        self.steps = 0
+        self.theta = 0.0
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+        self.diverge_penalty_ratio = 2.0
+
+        print("START Forward original Ant")
+        mujoco_env.MujocoEnv.__init__(self, "ant_3legged.xml", 5)
+        utils.EzPickle.__init__(self)        
+
+    def step(self, a):
+        self.steps += 1
+        
+        # no goal but constrain direction
+        xb = self.get_body_com("torso")[0]
+        yb = self.get_body_com("torso")[1]
+        self.do_simulation(a, self.frame_skip)
+        xa = self.get_body_com("torso")[0]
+        ya = self.get_body_com("torso")[1]
+        dx = xa - xb
+        dy = ya - yb
+        forward_reward = (dx * self.ct + dy * self.st) / self.dt
+        vertical_cost = (abs(-xa * self.st + ya * self.ct) - abs(-xb * self.st + yb * self.ct)) / self.dt
+        if vertical_cost > 0:
+            vertical_cost = vertical_cost * self.diverge_penalty_ratio
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - vertical_cost - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        done = not notdone
+        ob = self._get_obs()
+        if self.steps % 100 == 0:
+            print("[%d] %.2f %.2f %.2f" %(self.i_episode, self.get_body_com("torso")[0], self.get_body_com("torso")[1], math.atan2(self.get_body_com("torso")[1], self.get_body_com("torso")[0])))
+        return (
+            ob,
+            reward,
+            done,
+            dict(
+                reward_forward=forward_reward,
+                reward_vertical=-vertical_cost,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+
+    def _get_obs(self):
+        # single goal
+        x = self.sim.data.qpos.flat[0]
+        y = self.sim.data.qpos.flat[1]
+        r = math.sqrt(x ** 2 + y ** 2)
+        ct = x / r
+        st = y / r
+        return np.concatenate(
+            [
+                [r, ct, st],
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
+
+    def reset_model(self):
+        self.i_episode += 1
+        self.steps = 0
+        self.theta = 0.0
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+
+        # single goal
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+class Ant3leggedDiagonalOriginalPenaltyEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        self.i_episode = 0
+        self.max_steps = 1000
+        self.steps = 0
+        self.theta = math.pi / 4
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+        self.diverge_penalty_ratio = 1.0
+
+        print("START Forward original Ant")
+        mujoco_env.MujocoEnv.__init__(self, "ant_3legged.xml", 5)
+        utils.EzPickle.__init__(self)        
+
+    def step(self, a):
+        self.steps += 1
+        
+        # no goal but constrain direction
+        xb = self.get_body_com("torso")[0]
+        yb = self.get_body_com("torso")[1]
+        self.do_simulation(a, self.frame_skip)
+        xa = self.get_body_com("torso")[0]
+        ya = self.get_body_com("torso")[1]
+        dx = xa - xb
+        dy = ya - yb
+        forward_reward = (dx * self.ct + dy * self.st) / self.dt
+        vertical_cost = (abs(-xa * self.st + ya * self.ct) - abs(-xb * self.st + yb * self.ct)) / self.dt
+        if vertical_cost > 0:
+            vertical_cost = vertical_cost * self.diverge_penalty_ratio
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - vertical_cost - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        done = not notdone
+        ob = self._get_obs()
+        if self.steps % 100 == 0:
+            print("[%d] %.2f %.2f %.2f" %(self.i_episode, self.get_body_com("torso")[0], self.get_body_com("torso")[1], math.atan2(self.get_body_com("torso")[1], self.get_body_com("torso")[0])))
+        return (
+            ob,
+            reward,
+            done,
+            dict(
+                reward_forward=forward_reward,
+                reward_vertical=-vertical_cost,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+
+    def _get_obs(self):
+        # single goal
+        x = self.sim.data.qpos.flat[0]
+        y = self.sim.data.qpos.flat[1]
+        r = math.sqrt(x ** 2 + y ** 2)
+        ct = x / r
+        st = y / r
+        return np.concatenate(
+            [
+                [r, ct, st],
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
+
+    def reset_model(self):
+        self.i_episode += 1
+        self.steps = 0
+        self.theta = math.pi / 4
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+
+        # single goal
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+
+class Ant3leggedDiagonalUpdatedPenaltyEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        self.i_episode = 0
+        self.max_steps = 1000
+        self.steps = 0
+        self.theta = math.pi / 4
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+        self.diverge_penalty_ratio = 2.0
+
+        print("START Forward original Ant")
+        mujoco_env.MujocoEnv.__init__(self, "ant_3legged.xml", 5)
+        utils.EzPickle.__init__(self)        
+
+    def step(self, a):
+        self.steps += 1
+        
+        # no goal but constrain direction
+        xb = self.get_body_com("torso")[0]
+        yb = self.get_body_com("torso")[1]
+        self.do_simulation(a, self.frame_skip)
+        xa = self.get_body_com("torso")[0]
+        ya = self.get_body_com("torso")[1]
+        dx = xa - xb
+        dy = ya - yb
+        forward_reward = (dx * self.ct + dy * self.st) / self.dt
+        vertical_cost = (abs(-xa * self.st + ya * self.ct) - abs(-xb * self.st + yb * self.ct)) / self.dt
+        if vertical_cost > 0:
+            vertical_cost = vertical_cost * self.diverge_penalty_ratio
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - vertical_cost - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        done = not notdone
+        ob = self._get_obs()
+        if self.steps % 100 == 0:
+            print("[%d] %.2f %.2f %.2f" %(self.i_episode, self.get_body_com("torso")[0], self.get_body_com("torso")[1], math.atan2(self.get_body_com("torso")[1], self.get_body_com("torso")[0])))
+        return (
+            ob,
+            reward,
+            done,
+            dict(
+                reward_forward=forward_reward,
+                reward_vertical=-vertical_cost,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+
+    def _get_obs(self):
+        # single goal
+        x = self.sim.data.qpos.flat[0]
+        y = self.sim.data.qpos.flat[1]
+        r = math.sqrt(x ** 2 + y ** 2)
+        ct = x / r
+        st = y / r
+        return np.concatenate(
+            [
+                [r, ct, st],
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
+
+    def reset_model(self):
+        self.i_episode += 1
+        self.steps = 0
+        self.theta = math.pi / 4
+        self.st = math.sin(self.theta)
+        self.ct = math.cos(self.theta)
+
+        # single goal
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
