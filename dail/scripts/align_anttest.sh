f0438310dc93780dcfd50805582a19fde5ec3826
junseolee <greg3566@gmail.com>
2022-01-30 15:05:30 +0900

    GPU Setting for align_anttest.sh

--- dail/scripts/align_anttest.sh
+++ dail/scripts/align_anttest.sh
@@ -10,14 +10,15 @@ VENV_DIR="venv/bin/activate"
 tmux kill-session -t $SESS_NAME
 tmux new-session -d -s $SESS_NAME -n 1
 
-BEGIN=1
-END=1
+BEGIN=2
+END=3
 TOTAL_GPU=1
+GPUS=(0)
 
 for ((i=BEGIN; i<=END; i++)); do
-gpu_num=$((i % TOTAL_GPU))
+gpu_num=${GPUS[$((i % TOTAL_GPU))]}
 
-PYTHON_CMD="source ${VENV_DIR} && python train.py --algo ddpg --agent_type gama --load_dataset_dir ./expert_data/taskset/Antv1test.pickle --load_expert_dir None --save_learner_dir ./saved_alignments/Antv1test/seed_${i} --logdir ./logs/Antv1test/seed_${i} --edomain Antv1_1 --ldomain Antv1_1 --seed ${i} --gpu ${gpu_num}"
+PYTHON_CMD="source ${VENV_DIR} && CUDA_VISIBLE_DEVICES=${gpu_num} python train.py --algo ddpg --agent_type gama --load_dataset_dir ./expert_data/taskset/Antv1test.pickle --load_expert_dir None --save_learner_dir ./saved_alignments/Antv1test/seed_${i} --logdir ./logs/Antv1test/seed_${i} --edomain Antv1_1 --ldomain Antv1_1 --seed ${i} --gpu ${gpu_num}"
 
 if [ $i -ne $BEGIN ]
 then
